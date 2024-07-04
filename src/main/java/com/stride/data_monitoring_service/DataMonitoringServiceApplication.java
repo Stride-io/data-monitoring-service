c0609e072455db0ccc7d21c643ab017b781466ae
psawhney <psawhney@vmware.com>
2024-07-03 18:14:02 -0700

    Initial commit

---
+++ src/main/java/com/stride/data_monitoring_service/DataMonitoringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stride.data_monitoring_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataMonitoringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataMonitoringServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stride/data_monitoring_service/DataMonitoringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stride.data_monitoring_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMonitoringServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
